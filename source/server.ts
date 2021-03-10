232e1685d1394a787c960e04f9f88308c98d2a72
inamukhan-eforte <inam@eforte.net>
2021-03-10 06:15:59 -0500

    server side setup

---
+++ source/server.ts
@@ -0,0 +1,85 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import logging from './config/logging';
+import config from './config/config';
+import stationRoutes from './routes/station';
+import scheduler from './services/scheduler';
+import mongoose from 'mongoose';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
+import cors from 'cors';
+// let cors = require('cors');
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+scheduler();
+
+const NAMESPACE = 'Server';
+const router = express();
+// const MONGO_ATLAS: string = config.mongo.url;
+// const MONGO_ATLAS:string = `${process.env.MONGO_ATLAS}`;
+const MONGO_ATLAS:string = `mongodb+srv://iambuoyant:Gstc3hFmghzriTe@cluster0.1td2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+/** Connect to Mongo */
+mongoose
+    .connect(MONGO_ATLAS, config.mongo.options)
+    .then((result) => {
+        logging.info(NAMESPACE, 'Mongo Connected');
+    })
+    .catch((error) => {
+        logging.error(NAMESPACE, error.message, error);
+    });
+
+/** Parse the body of the request */
+// router.use(bodyParser.urlencoded({ extended: true }));
+// router.use(bodyParser.json());
+router.use(bodyParser.json({ limit: '900mb' }));
+router.use(bodyParser.urlencoded({ extended: true }));
+
+/* FOR CORS */
+router.use(cors());    
+    
+/** Log the request */
+router.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        /** Log the res */
+        logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    });
+
+    next();
+});
+
+
+
+router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+/** Rules of our API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+/** Routes go here */
+router.use('/api/v1/', stationRoutes);
+
+/** Error handling */
+router.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+
+router.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
